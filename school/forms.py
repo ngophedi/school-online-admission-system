3093535d125ebd64ed2e341181511b2be7ae11fa
ngophedi <khalebngophe@gmail.com>
2023-10-15 17:37:00 +0300

    first commit

---
+++ school/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+#for admin
+class AdminSigupForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+
+
+#for student related form
+class StudentUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','username','password']
+class StudentExtraForm(forms.ModelForm):
+    class Meta:
+        model=models.StudentExtra
+        fields=['roll','cl','mobile','fee','status']
+
+
+
+
+#for Attendance related form
+presence_choices=(('Present','Present'),('Absent','Absent'))
+class AttendanceForm(forms.Form):
+    present_status=forms.ChoiceField( choices=presence_choices)
+    date=forms.DateField()
+
+class AskDateForm(forms.Form):
+    date=forms.DateField()
+
+
+
+
+#for notice related form
+class NoticeForm(forms.ModelForm):
+    class Meta:
+        model=models.Notice
+        fields='__all__'
+
+
+
